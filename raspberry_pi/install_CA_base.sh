8c9ff8ab512915fe358beea8cc9719b8c9f85a34
jkcoop17 <jkcoop17@gmail.com>
2016-07-28 13:47:13 +0000

    assume yes for install script

--- raspberry_pi/install_CA_base.sh
+++ raspberry_pi/install_CA_base.sh
@@ -5,12 +5,12 @@
 apt-get update
 
 # Set up text editor
-apt-get install vim
+apt-get install -y vim
 cp vimrc ~/.vimrc
 
 # Install basic dev packages
-apt-get install python-dev=2.7.9-1
-apt-get install python-pip
+apt-get install -y python-dev=2.7.9-1
+apt-get install -y python-pip
 
 # Install dependencies
 pip install -I pymavlink==2.0.5 monotonic==0.6 pyserial==3.1.1
