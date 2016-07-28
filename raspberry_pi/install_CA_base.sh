976d7b55f017b202eda660bba1a92b234df52411
Kevin <jkcoop17@gmail.com>
2016-07-28 12:57:15 +0000

    Update install_CA_base.sh

--- raspberry_pi/install_CA_base.sh
+++ raspberry_pi/install_CA_base.sh
@@ -13,7 +13,7 @@ apt-get install python-dev=2.7.9-1
 apt-get install python-pip
 
 # Install dependencies
-pip install -I pymavlink==2.0.4 monotonic==0.6 pyserial==3.1.1
+pip install -I pymavlink==2.0.5 monotonic==0.6 pyserial==3.1.1
 
 # Install aircraft communication packages
 pip install -I dronekit==2.8.0 mavproxy==1.5.1
