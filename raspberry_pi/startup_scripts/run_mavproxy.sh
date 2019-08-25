03416c544cac10393fd4afc67ae1ed35fc7e0af0
Ronald Pandolfi <Ronald@KashmirWorldFoundation.org>
2019-08-25 18:58:47 -0400

    Delete run_mavproxy.sh

--- raspberry_pi/startup_scripts/run_mavproxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-screen -d -m -S "mavproxy.$(hostname)" \
-    mavproxy.py --aircraft=copter \
-    --master=/dev/ttyACM0 --baudrate=115200 \
-    --state-basedir=/home/pi/custom_aircraft/logs \
